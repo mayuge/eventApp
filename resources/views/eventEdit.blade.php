02bc8d57dda23f75ca6b369bacc7b05b1749940d
mayuge <kei.tomo.kimi.ken@gmail.com>
2023-02-14 18:57:47 +0900

    viewport

--- resources/views/eventEdit.blade.php
+++ resources/views/eventEdit.blade.php
@@ -2,7 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-       <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('/css/eventEdit.css')  }}" >
         <title>kimi ~君だけのイベントを開催しよう~</title>
     </head>
