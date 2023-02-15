ae620b3e4c1663bcdfd1612b0c35e4308a1d2caf
mayuge <kei.tomo.kimi.ken@gmail.com>
2023-02-15 16:13:21 +0900

    ssl対応2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \URL::forceScheme('https'); 
+         if (\App::environment(['production'])) {
+                \URL::forceScheme('https');
+            }
        // Paginator::useBootstrap();
         Paginator::defaultView('vendor.pagination.bootstrap-4');
     }
