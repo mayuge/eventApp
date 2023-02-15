22f3b6a6a6588c28cd498529e3e059ea9cb4bd4e
mayuge <kei.tomo.kimi.ken@gmail.com>
2023-02-15 16:04:08 +0900

    強制SSL化

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
+        \App\Http\Middleware\ForceHttps::class, // 追加
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
