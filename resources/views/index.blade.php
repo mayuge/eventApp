dab40f5b33b5847b790fe08ebbee597013ade950
mayuge <kei.tomo.kimi.ken@gmail.com>
2023-02-15 16:23:47 +0900

    レスポンシブ対4

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('/css/index.css')  }}" >
+        <link rel="stylesheet" href="{{ asset('/css/loginBlog.css')  }}" >
         <title>kimi ~君だけのイベントを開催しよう~</title>
     </head>
     <body class="antialiased">
